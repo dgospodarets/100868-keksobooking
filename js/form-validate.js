4577f0755e9a765518a0403d97a1ef2a08c44805
Igor Alexeenko <alexeenko.igor@gmail.com>
2015-09-18 21:53:50 +0300

    Ограничил область видимости

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,87 +1,89 @@
-var addLeadingZero = function(value) {
-  if (value < 10) {
-    return '0' + value;
-  }
-
-  return '' + value;
-};
-
-var getFormattedDate = function(date) {
-  if (typeof date === 'undefined') {
-    date = new Date();
-  }
-
-  var fullMonth = addLeadingZero(date.getMonth() + 1);
-  var fullDate = addLeadingZero(date.getDate());
-
-  return [date.getFullYear(), fullMonth, fullDate].join('-');
-};
-
-var restoreFormValueFromCookies = function(form) {
-  var element;
-  for (var i = 0, l = form.elements.length; i < l; i++) {
-    element = form.elements[i];
-
-    if (docCookies.hasItem(element.name)) {
-      element.value = docCookies.getItem(element.name);
+(function() {
+  var addLeadingZero = function(value) {
+    if (value < 10) {
+      return '0' + value;
     }
+
+    return '' + value;
   };
-};
 
-var formElement = document.forms['searchform'];
+  var getFormattedDate = function(date) {
+    if (typeof date === 'undefined') {
+      date = new Date();
+    }
 
-var dateFrom = formElement['form-from'];
-var dateTo = formElement['form-to'];
-var guestsNumber = formElement['searchform-guests-number'];
-var roomsNumber = formElement['searchform-guests-rooms'];
+    var fullMonth = addLeadingZero(date.getMonth() + 1);
+    var fullDate = addLeadingZero(date.getDate());
 
-var MAX_GUESTS_AVAILABLE = 6;
-var MAX_GUESTS_PER_ROOM = 3;
-var MIN_GUESTS_PER_ROOM = 1;
-var MILLISECONDS_IN_DAY = 60 * 60 * 24 * 1000;
+    return [date.getFullYear(), fullMonth, fullDate].join('-');
+  };
 
-var minimalDifference = MILLISECONDS_IN_DAY;
+  var restoreFormValueFromCookies = function(form) {
+    var element;
+    for (var i = 0, l = form.elements.length; i < l; i++) {
+      element = form.elements[i];
 
-dateFrom.min = getFormattedDate();
-guestsNumber.min = roomsNumber.min = 1;
+      if (docCookies.hasItem(element.name)) {
+        element.value = docCookies.getItem(element.name);
+      }
+    };
+  };
 
-dateFrom.value = getFormattedDate();
-dateTo.value = getFormattedDate(new Date(new Date(dateFrom.value) + minimalDifference));
-guestsNumber.value = 2;
-roomsNumber.value = 1;
+  var formElement = document.forms['searchform'];
 
-restoreFormValueFromCookies(formElement);
+  var dateFrom = formElement['form-from'];
+  var dateTo = formElement['form-to'];
+  var guestsNumber = formElement['searchform-guests-number'];
+  var roomsNumber = formElement['searchform-guests-rooms'];
 
-dateFrom.onchange = function(evt) {
-  var dateFromValue = new Date(dateFrom.value);
-  var dateToValue = new Date(dateTo.value);
-  var minimalToDate = getFormattedDate(new Date(+dateFromValue + minimalDifference));
+  var MAX_GUESTS_AVAILABLE = 6;
+  var MAX_GUESTS_PER_ROOM = 3;
+  var MIN_GUESTS_PER_ROOM = 1;
+  var MILLISECONDS_IN_DAY = 60 * 60 * 24 * 1000;
 
-  if (dateToValue - dateFromValue < minimalDifference) {
-    dateTo.value = minimalToDate;
-  }
+  var minimalDifference = MILLISECONDS_IN_DAY;
 
-  dateTo.min = minimalToDate;
-};
+  dateFrom.min = getFormattedDate();
+  guestsNumber.min = roomsNumber.min = 1;
 
-guestsNumber.onchange = function(evt) {
-  roomsNumber.min = parseInt(parseInt(guestsNumber.value, 10) / MAX_GUESTS_PER_ROOM);
-  roomsNumber.max = parseInt(parseInt(guestsNumber.value, 10) / MIN_GUESTS_PER_ROOM);
-};
+  dateFrom.value = getFormattedDate();
+  dateTo.value = getFormattedDate(new Date(new Date(dateFrom.value) + minimalDifference));
+  guestsNumber.value = 2;
+  roomsNumber.value = 1;
 
-roomsNumber.onchange = function(evt) {
-  guestsNumber.min = parseInt(roomsNumber.value, 10) * MIN_GUESTS_PER_ROOM;
-  guestsNumber.max = parseInt(roomsNumber.value, 10) * MAX_GUESTS_PER_ROOM;
-};
+  restoreFormValueFromCookies(formElement);
 
-formElement.onsubmit = function(evt) {
-  evt.preventDefault();
+  dateFrom.onchange = function(evt) {
+    var dateFromValue = new Date(dateFrom.value);
+    var dateToValue = new Date(dateTo.value);
+    var minimalToDate = getFormattedDate(new Date(+dateFromValue + minimalDifference));
 
-  var element;
-  for (var i = 0, l = formElement.elements.length; i < l; i++) {
-    element = formElement.elements[i];
-    docCookies.setItem(element.name, element.value);
-  }
+    if (dateToValue - dateFromValue < minimalDifference) {
+      dateTo.value = minimalToDate;
+    }
 
-  formElement.submit();
-};
+    dateTo.min = minimalToDate;
+  };
+
+  guestsNumber.onchange = function(evt) {
+    roomsNumber.min = parseInt(parseInt(guestsNumber.value, 10) / MAX_GUESTS_PER_ROOM);
+    roomsNumber.max = parseInt(parseInt(guestsNumber.value, 10) / MIN_GUESTS_PER_ROOM);
+  };
+
+  roomsNumber.onchange = function(evt) {
+    guestsNumber.min = parseInt(roomsNumber.value, 10) * MIN_GUESTS_PER_ROOM;
+    guestsNumber.max = parseInt(roomsNumber.value, 10) * MAX_GUESTS_PER_ROOM;
+  };
+
+  formElement.onsubmit = function(evt) {
+    evt.preventDefault();
+
+    var element;
+    for (var i = 0, l = formElement.elements.length; i < l; i++) {
+      element = formElement.elements[i];
+      docCookies.setItem(element.name, element.value);
+    }
+
+    formElement.submit();
+  };
+})();
